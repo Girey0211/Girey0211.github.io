7f8306d0aa37e8c4a4e1457e6113197529eba603
Girey0211 <101105181+Girey0211@users.noreply.github.com>
2022-07-12 09:41:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ class App{
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
 
-        this.ball = new Ball(this.stageWidth, this.stageHeight, 60, 15);
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 40, 15);
         this.block = new Block(500, 30, 300, 350);
 
         window.requestAnimationFrame(this.animate.bind(this));
